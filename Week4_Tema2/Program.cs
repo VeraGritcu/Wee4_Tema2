f7c01173e58f197eb2e0294048c3f9662082f380
Vera <Vera@DESKTOP-PBQVJHH>
2019-11-02 19:02:48 +0200

    update

---
+++ Week4_Tema2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_Tema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myQueue = new Queue(1);
+            myQueue.Enqueue(2);
+            myQueue.Enqueue(3);
+            myQueue.Enqueue(4);
+            myQueue.Dequeue();
+            myQueue.Print();
+            Console.ReadKey();
+
+            
+        }
+    }
+}
---
+++ Week4_Tema2/Queue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_Tema2
+{
+    internal class Queue
+    {
+        public Queue(object x)
+        {
+            QueueSequence = new LinkedList<object>();
+            QueueSequence.AddFirst(x);
+        }
+        private LinkedList<object> QueueSequence { get; set; }
+
+        internal void Enqueue(object x)
+        {
+            QueueSequence.AddFirst(x);
+        }
+
+        internal void Dequeue()
+        {
+            QueueSequence.RemoveLast();
+        }
+
+        internal void Print()
+        {
+            foreach (object item in QueueSequence)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
